207ab3fe77d5127c6b2a6224b8c9f950b909bd6e
amorandy <antoniomorandy@hotmail.com>
2026-05-11 15:46:24 -0400

    Update API base URL for PetShopApi

--- PetShopApi/script/index.js
+++ PetShopApi/script/index.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://require-radios-assists-local.trycloudflare.com/api";
+const API_BASE_URL = "https://lucky-humans-visit.loca.lt/api";
 function decodeJwtResponse(token) {
     let base64Url = token.split('.')[1];
     let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
