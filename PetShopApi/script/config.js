620bb5ba6293628e59cc5e210ab77ecd704e80d6
amorandy <antoniomorandy@hotmail.com>
2026-05-28 09:29:14 -0400

    cambios url base

--- PetShopApi/script/config.js
+++ PetShopApi/script/config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-    API_BASE_URL: "https://era-verification-rod-underwear.trycloudflare.com/api" 
+    API_BASE_URL: "https://calculate-external-transportation-goes.trycloudflare.com/api" 
 };
