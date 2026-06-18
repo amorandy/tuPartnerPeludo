bc6ef3bbb8bc59c7059787f140d4325bfd3ef616
amorandy <antoniomorandy@hotmail.com>
2026-06-18 11:26:05 -0400

    actualizo passSMTP

--- PetShopApi/Models/RecuperarRequest.cs
+++ PetShopApi/Models/RecuperarRequest.cs
@@ -4,6 +4,6 @@
     {
         public string? Telefono { get; set; }
         public string? Email { get; set; }
-        public string Metodo { get; set; } // "WHATSAPP" o "EMAIL"
+        public string? Metodo { get; set; } // "WHATSAPP" o "EMAIL"
     }
 }
