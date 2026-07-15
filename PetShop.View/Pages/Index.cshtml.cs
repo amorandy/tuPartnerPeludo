9287d8ef512cd284f78a9f5c143fa07cd710cd60
amorandy <antoniomorandy@hotmail.com>
2026-07-15 11:15:26 -0400

    Reconstrucción de proyecto PetShop.View tras error de configuración y limpieza de archivos temporales

---
+++ PetShop.View/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PetShop.View.Nuevo.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
